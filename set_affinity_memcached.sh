168f5259d5007d7aaffe248c17df1fae71e2fdd6
Vlad Zolotarov <vladz@cloudius-systems.com>
2014-07-31 17:28:21 +0300

    set_affinity_qemu.sh: configures the affinity for a qumu guest:
    - Uses all but 2 last CPUs in the host for qemu threads binding each of them to a single CPU.
    - Disables irqbalance daemon.
    - Binds vhost to a separate CPU (one before the last).
    - Binds all IRQs of a given NIC to the last CPU.
    
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>
    
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

---
+++ set_affinity_memcached.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# !
+# !  Usage: ./set_affinity_memcached.sh <host net interface, e.g. eth1>
+# !
+
+if [[ $# -ne 1 ]]; then
+    cat $0 | grep ^"# !" | cut -d"!" -f2-
+    exit 1
+fi
+
+cur_cpu=0
+total_cpus=`cat /proc/cpuinfo | grep processor | wc -l`
+#total_cpus=1
+NIC=$1
+cpus4qemu=$((total_cpus-2))
+if [[ $cpus4qemu -le 0 ]]; then
+	cpus4qemu=1
+fi
+
+echo "cpus4qemu=$cpus4qemu"
+
+# Set qemu threads affinity
+for t in $(taskset -ap `pgrep qemu-system` | cut -d" " -f2 | cut -d"'" -f1)
+do
+    sudo taskset -pc $cur_cpu $t
+    cur_cpu=$(((cur_cpu + 1) % cpus4qemu))
+done
+
+cur_cpu=$((cpus4qemu % total_cpus))
+
+echo "Setting the vhost affinity to CPU $cur_cpu"
+sudo taskset -pc $cur_cpu `pgrep vhost`
+
+echo "Stoping irqbalance.."
+sudo systemctl stop irqbalance.service
+
+cur_cpu=$(((cur_cpu+1) % total_cpus))
+
+# Set the affinity of the NIC irqs to the same 
+for i in `grep $NIC /proc/interrupts | awk -F: '{print $1}'`
+do
+    echo "Setting the affinity of IRQ $i to CPU $cur_cpu"
+    sudo sh -c "echo $cur_cpu > /proc/irq/$i/smp_affinity_list"
+done
+
